5955a852434f0933fe0713acbdd217abf734e721
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-20 21:33:24 +0200

    map.py

--- map.py
+++ map.py
@@ -5,3 +5,13 @@ nums = [24, 28, 19, 179]
 result = list(map(is_devide_by_three, nums)) 
 
 print(result) 
+
+def my_func(a, b):
+    return a*b
+ 
+nums1 = [6, 7, 8, 9, 10]
+nums2 = [1, 2, 3, 4, 5]
+ 
+nums_multiply = list(map(my_func, nums1, nums2))
+ 
+print(nums_multiply)
