43d6bc021d775b22dbab4f800d53fadda01ac62e
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-20 21:35:56 +0200

    nonlocal.py

--- nonlocal.py
+++ nonlocal.py
@@ -2,11 +2,16 @@ counter = 0
 
 def create_scope(default):
     counter = default * 2
-    
+ 
     def nonlocal_print():
-        nonlocal_counter
-        print(f"scoped {counter}) 
-
-    def global_print(): 
+        nonlocal counter
+        print(f"scoped {counter = }")
+ 
+    def global_print():
         global counter
-        print(f"global {counter}")
+        print(f"global {counter = }")
+ 
+    nonlocal_print()
+    global_print()
+ 
+create_scope(-4) 
