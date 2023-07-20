ebb83d28c34374ac143c87f16b3de322e88ea98b
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-20 21:25:46 +0200

    listcomp.py

---
+++ listcomp.py
@@ -0,0 +1,10 @@
+symbols = 'Python'
+symbol_codes = [ord(symbol) for symbol in symbols]
+print(symbol_codes) 
+
+symbols = 'snake'
+symbol_codes = [ord(symbol) for symbol in symbols) 
+print(symbol_codes) 
+
+for object in symbol_codes:
+    print(object)
