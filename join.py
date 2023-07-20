88efc155572942e3be4be32531caf70d8e7360f0
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-20 21:22:29 +0200

    join.py

--- join.py
+++ join.py
@@ -1,14 +1,3 @@
 glasnie = ['а', 'у' ]
-glasnie_str  = "==".join(glasnie) 
+glasnie_str = "==".join(glasnie) 
 print("Строка гласных:", glasnie_str) 
-
-
-upper_lower.py
-
-s = 'hEllo_world'
-su = s.upper() 
-sl = s.lower() 
-
-print(s) 
-print(su) 
-print(sl) 
