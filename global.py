9220a8ffccd4296a4f6c7a65f84873ccf364aa13
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-20 21:34:38 +0200

    global.py

--- global.py
+++ global.py
@@ -1,8 +1,14 @@
 counter = 0
-
-def update(value): 
+ 
+def update(value):
     global counter
     result = counter + value
-    
-    print(f"{counter} + {value} + {result}") 
+ 
+    print(f"{counter} + {value} = {result}")
     counter = result
+ 
+update(1)
+update(5)
+update(3)
+ 
+print(f"{counter = }")
