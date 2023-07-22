2df576d4f8f733e188019bf546cd3272a4d2a781
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-22 15:07:11 +0200

    lab_5_task_2.py

---
+++ lab_5_task_2.py
@@ -0,0 +1,41 @@
+[ 22 июля 2023 г. 0:52 ] ⁨Олег⁩: name = 'Smirnov Oleg'
+a = []
+b = ''
+c = ''
+d = []
+e = ''
+h = 0
+t = 0
+for symbol in name:
+    b += symbol + '_'
+print(b)
+
+for symbol in b:
+    if symbol == 95:
+        c = c
+    elif ord(symbol) > 64 and ord(symbol) < 97 or ord(symbol) == 32:
+        c += symbol
+    else:
+        c += chr(ord(symbol) - 32)
+    a.append(ord(c[h]))
+    h += 1
+
+print(a)
+
+for symbol in b:
+    if symbol == 95:
+        e = e
+    elif ord(symbol) > 97 or ord(symbol) == 32:
+        e += symbol
+    else:
+        e += chr(ord(symbol) + 32)
+    d.append(ord(e[t]))
+    t += 1
+
+print(d)
+
+print(f'Максимальное значение из списка с верхним регистром: {max(a)}')
+print(f'Максимальное значение из списка с нижним регистром: {max(d)}')
+
+#print(f'Максимальное значение из списка с верхним регистром: {ord(max(c))}')
+#print(f'Максимальное значение из списка с нижним регистром: {ord(max(e))}')
