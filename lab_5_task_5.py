605b5f3af04bc2c54ad61823def771f419e8365d
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-22 15:08:33 +0200

    lab_5_task_5.py

---
+++ lab_5_task_5.py
@@ -0,0 +1,35 @@
+[ 22 июля 2023 г. 0:57 ] ⁨Олег⁩: name = 'Smirnov Oleg Vladimirovich'
+a = []
+b = ''
+c = ''
+d = []
+e = ''
+h = 0
+t = 0
+
+for symbol in name:
+    if symbol == 95:
+        c = c
+    elif ord(symbol) > 64 and ord(symbol) < 97 or ord(symbol) == 32:
+        c += symbol
+    else:
+        c += chr(ord(symbol) - 32)
+    a.append(ord(c[h]))
+    h += 1
+
+print(a)
+
+for symbol in name:
+    if symbol == 95:
+        e = e
+    elif ord(symbol) > 97 or ord(symbol) == 32:
+        e += symbol
+    else:
+        e += chr(ord(symbol) + 32)
+    d.append(ord(e[t]))
+    t += 1
+
+print(d)
+
+print(f'Сумма значений из списка с верхним регистром: {sum(a)}')
+print(f'Сумма значений из списка с нижним регистром: {sum(d)}')
