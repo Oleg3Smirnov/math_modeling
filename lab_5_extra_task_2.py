eaaf78b519012e28f9a628da14fdac458927e68a
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-22 15:10:37 +0200

    lab_5_extra_task_2.py

---
+++ lab_5_extra_task_2.py
@@ -0,0 +1,29 @@
+import random 
+
+n = random.randint(2, 50)
+x = random.randint(1, n*8//10)
+a = []
+
+print(f'n = {n}')
+print(f'Количество элементов в списке: {x}')
+print('Числа в списке могут повторяться')
+for i in range(0, x, 1):
+    b = random.randint(1, 100)
+    a.append(b)
+print(f'Список: {a}')
+
+c = random.randint(1, n-1)
+
+h = 0
+while h < 1:
+    e = 1
+    f = 1
+    for i in range(0, x, 1):
+        if c == a[i]:
+            e += 1
+    if e == f:
+        h += 1
+    else:
+        c = random.randint(1, n-1)
+
+print(f'Рандомное число: {x}')
