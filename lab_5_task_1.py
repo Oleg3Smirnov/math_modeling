bb4f99f3771d1795e164d3d0bc3eebd0c9c0e044
Oleg2Smirnov <137449733+Oleg2Smirnov@users.noreply.github.com>
2023-07-25 01:42:52 +0200

    lab_5_task_1.py

--- lab_5_task_1.py
+++ lab_5_task_1.py
@@ -1,19 +1,32 @@
-import random 
+import random
 
-a = random.randint(1, 100)
-b = []
-c = []
-d = []
-e = []
+import numpy as np
 
-for i in range(0, 100, 1):
-    b.append(random.randint(0, 100))
-    c.append(random.randint(0, 100))
-    d.append(random.randint(0, 100))
+N = random.randint(1, 100)
+c = np.zeros((1, N))
+d = np.zeros((1, N))
+e = np.zeros((1, N))
 
-e.append(max(b))
-e.append(max(c))
-e.append(max(d))
+for i in range(0, N, 1):
+    c[0, i] = random.randint(1, 100)
 
-print(f'Самый большой элемент: {max(e)}')
-print(f'Сумма массивов: {sum(b) + sum(c) + sum(d)}')
+for i in range(0, N, 1):
+    d[0, i] = random.randint(1, 100)
+
+for i in range(0, N, 1):
+    e[0, i] = random.randint(1, 100)
+
+print(c)
+print(d)
+print(e)
+
+h = max(c[0])
+if h < max(d[0]):
+    h = max(d[0])
+    if h < max(e[0]):
+        h = max(e[0])
+elif h < max(e[0]):
+    h = max(e[0])
+
+print(f'наибольший элемент среди всех трех массивов: {h}')
+print(f'сумма всех элементов созданных массивов: {sum(c[0]) + sum(d[0]) + sum(e[0])}')
