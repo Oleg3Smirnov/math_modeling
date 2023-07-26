83ce90d32424aab09b532485b9c7b05185f85842
Oleg3Smirnov <139594167+Oleg3Smirnov@users.noreply.github.com>
2023-07-26 23:37:22 +0200

    lab_5_task_5.py

--- lab_5_task_5.py
+++ lab_5_task_5.py
@@ -1,34 +1,19 @@
 name = 'Smirnov Oleg Vladimirovich'
 a = []
-b = ''
-c = ''
 d = []
-e = ''
-h = 0
-t = 0
+
+name = '_'.join(name)
+print(name)
+
+name = name.upper()
 
 for symbol in name:
-    if symbol == 95:
-        c = c
-    elif ord(symbol) > 64 and ord(symbol) < 97 or ord(symbol) == 32:
-        c += symbol
-    else:
-        c += chr(ord(symbol) - 32)
-    a.append(ord(c[h]))
-    h += 1
-
+   a.append(ord(symbol))
 print(a)
 
+name = name.lower()
 for symbol in name:
-    if symbol == 95:
-        e = e
-    elif ord(symbol) > 97 or ord(symbol) == 32:
-        e += symbol
-    else:
-        e += chr(ord(symbol) + 32)
-    d.append(ord(e[t]))
-    t += 1
-
+    d.append(ord(symbol))
 print(d)
 
 print(f'Сумма значений из списка с верхним регистром: {sum(a)}')
